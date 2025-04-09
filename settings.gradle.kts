19a2ebf281ecc7c17a63f891797b85e179e17ec7
Roman Rastiehaiev <roman.rastiehaiev@hibob.io>
2025-04-09 18:22:38 +0200

    Fix names.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "suspensify-kotlin-compiler-plugin"
+rootProject.name = "kotlin-compiler-plugin-template"
 
 include("plugin-gradle")
 include("plugin-kotlin")
